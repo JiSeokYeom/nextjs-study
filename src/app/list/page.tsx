0acd9ef6b667664dd90d8314780bdae07e180807
jiseok <ygs0103>
2023-05-18 00:01:35 +0900

    feat: 라우팅 기능까지 공부

---
+++ src/app/list/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <h4 className="title"> 이것은 두번째 스터디용 페이지 입니다</h4>
+            </div>
+            <div className="data">
+                <h4>데이터1</h4>
+            </div>
+            <div className="data">
+                <h4>데이터2</h4>
+            </div>
+        </div>
+    )
+}
