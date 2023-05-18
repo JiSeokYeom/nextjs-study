2e74b589759c8e5e1d99309ede52718b717a1190
jiseok <ygs0103>
2023-05-18 12:19:31 +0900

    컴포넌트,이미지 추가 까지 학습

---
+++ src/app/list/data.js
@@ -0,0 +1,3 @@
+let datas = ['data1', 'data2', 'data3'];
+export default datas
+// 여러개 하고 싶으면 export {변수 or 함수명, 변수 or 함수명}
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,16 +1,34 @@
+import Image from 'next/image'
+import image2 from '/public/test.png'
+import datas from './data.js'
 
 export default function Home() {
+
     return (
         <div>
             <div>
                 <h4 className="title"> 이것은 두번째 스터디용 페이지 입니다</h4>
-            </div>
-            <div className="data">
-                <h4>데이터1</h4>
-            </div>
-            <div className="data">
-                <h4>데이터2</h4>
+                {
+                    datas.map((data,i) => {
+                        return (
+                            SampleData(data = data)
+                            // <div className="data" key={i}>
+                            //     <Image src={image2} alt= "고양이" className="testImage"/>
+                            //     <h4>{data}</h4>
+                            // </div>
+                        )
+                    })
+                }
             </div>
         </div>
+    )
+}
+
+function SampleData(data: String){
+    return (
+        <div className="data">
+        <Image src={image2} alt= "고양이" className="testImage"/>
+        <h4>{data}</h4>
+    </div>
     )
 }
